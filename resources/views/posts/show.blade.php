446067f14c844427c528a6c81c7a87617408e6d4
Muhammad Mahin Dar <muhammadmahindar@gmail.com>
2019-01-13 01:55:24 +0500

    Fixed Changes while showing post to logged out user

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -9,43 +9,49 @@
                     <div class="panel-heading">Post Detail</div>
 
                     <div class="panel-body">
-                        <h2>Post Detail 
-                            <a type="submit" style="float: right" class="btn btn-primary" href="{{ route('post.index') }}">Back</a>
+                        <h2>Post Detail
+                            <a type="submit" style="float: right" class="btn btn-primary"
+                               href="{{ route('post.index') }}">Back</a>
                         </h2><br>
-
 
 
                         <b>Title:</b> {{ $post->title }}<br>
                         <b>Description:</b> {{ $post->description }}<br>
 
                         <br>
-                    <div class="panel-footer">
-                    <h4><b>COMMENTS</b></h4>
-                    <ul>
-                    @foreach($post->comments as $comment)
-                    <li>
-                        {{ $comment->comment}}<br>
-                        <strong> {{ $comment->user->name }} </strong>
-                            {{\carbon\Carbon::parse($comment->created_at)->diffForHumans() }}
-                    </li>
-                    @endforeach
-                    </ul>
-                    </div>
-                    <div class="Add comment">
-                    <h4>Add Comment</h4>
-                    <form method="POST" action="{{ route('comment.store') }}">
-                        <input type="hidden" name="post_id" value="{{ $post->id }}" />
-                        {!! csrf_field() !!}
-                        <div class="form-group">
-                            <input class="form-control" type="text" name="comment"required  />
+                        <div class="panel-footer">
+                            <h4><b>COMMENTS</b></h4>
+                            <ul>
+                                @foreach($post->comments as $comment)
+                                    <li>
+                                        {{ $comment->comment}}<br>
+                                        <strong> {{ $comment->user->name }} </strong>
+                                        {{\carbon\Carbon::parse($comment->created_at)->diffForHumans() }}
+                                    </li>
+                                @endforeach
+                            </ul>
                         </div>
-                        <div>
-                            
-                            <input type="submit" class="btn btn-warning" value=" Comment" required />
-                        </div>
-                    </form>
-                    </div>
-                        
+                        @guest
+                            <button class="btn btn-danger"><a href="/login">Login to comment</a></button>
+                        @else
+
+                            <div class="Add comment">
+                                <h4>Add Comment</h4>
+                                <form method="POST" action="{{ route('comment.store') }}">
+                                    <input type="hidden" name="post_id" value="{{ $post->id }}"/>
+                                    {!! csrf_field() !!}
+                                    <div class="form-group">
+                                        <input class="form-control" type="text" name="comment" required/>
+                                    </div>
+                                    <div>
+
+                                        <input type="submit" class="btn btn-warning" value=" Comment" required/>
+                                    </div>
+                                </form>
+                            </div>
+                        @endguest
+
+
                         <br>
 
                     </div>
